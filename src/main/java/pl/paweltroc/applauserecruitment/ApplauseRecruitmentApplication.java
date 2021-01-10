71a06ce18577c7bd1014a44cebd879740b37919a
Pawel Troc <aliasef94@gmail.com>
2021-01-10 16:45:55 +0100

    Initial backend setup

---
+++ src/main/java/pl/paweltroc/applauserecruitment/ApplauseRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package pl.paweltroc.applauserecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplauseRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplauseRecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/paweltroc/applauserecruitment/ApplauseRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.paweltroc.applauserecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplauseRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
